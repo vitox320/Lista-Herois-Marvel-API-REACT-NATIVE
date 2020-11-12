ca4e2e3333c3bfb198a76b3f8875af304bbf010a
João Victor <vitox320@gmail.com>
2020-11-11 23:40:46 -0300

    Testando react-native

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import Home from './src/screens/Home'
+import Description from './src/screens/Description'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+/*function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
+  );
+}*/
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen name="Heroes" component={Home} />
+        <Stack.Screen name="Description" component={Description}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default App
+
+/*
+export default App;
+
+const App = StackNavigator({
+  Home: {
+    screen: Home
   },
-});
+  Description: {
+    screen: Description
+  },
+},
+{
+  cardStyle: {
+    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+  }
+})
+export default App
+*/
+
+
