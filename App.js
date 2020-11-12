367781ada93881f9e65e7010afd63b8a82532d46
João Victor <vitox320@gmail.com>
2020-11-12 11:57:25 -0300

    Adicionando mais info na descricao e Drawernavigation

--- App.js
+++ App.js
@@ -2,50 +2,27 @@ import * as React from 'react';
 import Home from './src/screens/Home'
 import Description from './src/screens/Description'
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-/*function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}*/
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 
-const Stack = createStackNavigator();
 
+
+
+
+const Drawer = createDrawerNavigator();
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName = "Home">
-        <Stack.Screen name="Heroes" component={Home} />
-        <Stack.Screen name="Description" component={Description}/>
-
-      </Stack.Navigator>
-    </NavigationContainer>
+  return (  
+    
+      
+          <NavigationContainer >
+            <Drawer.Navigator initialRouteName="Home">
+              <Drawer.Screen name="Heroes" component={Home} />
+              <Drawer.Screen name="Description" component={Description} />
+            </Drawer.Navigator>
+          </NavigationContainer>
+       
+    
+     
   );
 }
-
 export default App
-
-/*
-export default App;
-
-const App = StackNavigator({
-  Home: {
-    screen: Home
-  },
-  Description: {
-    screen: Description
-  },
-},
-{
-  cardStyle: {
-    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
-  }
-})
-export default App
-*/
-
-
--- src/screens/Description.js
+++ src/screens/Description.js
@@ -3,7 +3,6 @@ import { ScrollView, Image, Dimensions, Text,SafeAreaView } from 'react-native'
 
 const SCREEN_WIDTH = Dimensions.get('screen').width
 
-
   
     function Description({ route }) {
         const { hero } = route.params;
@@ -14,9 +13,15 @@ const SCREEN_WIDTH = Dimensions.get('screen').width
                     source={{uri: `${hero.thumbnail.path}.${hero.thumbnail.extension}`}} 
                     style={{width:SCREEN_WIDTH, height:SCREEN_WIDTH}}  
                 />
-        
+                                
                 <Text style={{padding:10, fontSize:20}}>{hero.name}</Text>
-                <Text style={{padding:10}}>{hero.description}</Text>       
+                <Text style={{padding:10,fontSize:20}}> Id do personagem: {hero.id}</Text> 
+                <Text style={{padding:10}}>{hero.description}</Text>  
+                <Text style={{padding:10}}>Aparições em histórias em quadrinhos:  {hero.comics.available}</Text>  
+                <Text style={{padding:10}}>Aparições em séries : {hero.series.available}</Text>
+                <Text style={{padding:10}}>Aparições em Histórias : {hero.stories.available}</Text>    
+                
+                     
             </ScrollView>
           </SafeAreaView>
         );        
